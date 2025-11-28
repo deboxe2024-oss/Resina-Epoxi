e0bdac16ed2058d1e4243478f3a19c7f06dbae79
Lucas <kamakurashorts@gmail.com>
2025-11-28 16:08:01 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -39,7 +37,13 @@ const nextConfig: NextConfig = {
         hostname: 'nwuievvpcjrmecujwfox.supabase.co',
         port: '',
         pathname: '/**',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'kdloteojnkcjblhoirea.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
