0a9202a0f77c28fe98c60fc8536ca54613b3402f
Lucas <kamakurashorts@gmail.com>
2025-12-04 22:15:27 +0000

    <!-- Meta Pixel Code Anti-Duplo --> <script>   !function(f,b,e,v,n,t,s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,38 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <Script
+          id="meta-pixel-anti-duplo"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '689670297299694', {
+                external_id: '',
+                eventSource: 'website'
+              });
+
+              fbq('track', 'PageView');
+              fbq('track', 'ViewContent');
+
+              fbq('disablePushState', true);
+              fbq('set', 'autoConfig', false, '689670297299694');
+              window.fbq = fbq;
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+          src="https://www.facebook.com/tr?id=689670297299694&ev=PageView&noscript=1"/>
+        </noscript>
+        <Script
           id="utmify-utms"
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
