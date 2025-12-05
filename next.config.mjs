4b65b7eacb23bc7d2a56cef9b392307b35b95e00
Lucas <kamakurashorts@gmail.com>
2025-12-05 02:51:03 +0000

    Voce vai montar o site com as imagens em tamanho de site
    https://nwui

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'joiasbotanicas.com.br',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-             {
-                protocol: 'https',
-                hostname: 'hotmart.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nwuievvpcjrmecujwfox.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
